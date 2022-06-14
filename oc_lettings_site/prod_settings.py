043b7c3e8060d8faaaf541a661e015fc7df7d217
Orphée <letembeorphee@gmail.com>
2022-06-14 20:54:16 +0200

    fix linting settings

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -68,7 +68,8 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-""" 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
