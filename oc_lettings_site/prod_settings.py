ef55b4a6afbd4665e6ce5c9ab940a12eade4cd8a
Orphée <letembeorphee@gmail.com>
2022-06-14 15:02:44 +0200

    add python decouple

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,3 +1,18 @@
+
+from oc_lettings_site.settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['oc-lettings-01.herokuapp.com']
+
+
+
+
+"""
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -126,3 +141,4 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     send_default_pii=True
 )
+"""
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,8 @@
 import os
+from decouple import config
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -109,3 +113,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# sentry configuration
+sentry_sdk.init(
+    dsn="https://68f7603144744132b7fd982f3005f291@o1284738.ingest.sentry.io/6495612",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
+
+
+
+
