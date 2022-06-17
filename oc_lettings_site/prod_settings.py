162c20b33f1b4310d86cbaa1a5c1d952b86b6c9d
Orphée <letembeorphee@gmail.com>
2022-06-17 13:53:03 +0200

    add dumps data to heroku db

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,11 +1,11 @@
 
 import os
-
+import  dj_database_url
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
+from oc_lettings_site.settings import DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -72,14 +72,15 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+DATABASES['default'] = dj_database_url.config()
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
